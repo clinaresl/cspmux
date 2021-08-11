f2be840c9bc5031a4458d47d849d9dafb31e40e9
Carlos Linares López <carlos.linares@uc3m.es>
2021-08-11 23:47:05 +0200

    Unit tests over string and time managers

--- tests/fixtures/TSTmanagerfixture.h
+++ tests/fixtures/TSTmanagerfixture.h
@@ -40,7 +40,7 @@ class ManagerFixture : public ::testing::Test {
         // return a randomly generated vector of strings and integers to
         // populate the domain of each variable. The number of values generated
         // is also randomly selected in the interval [0, MAX_LENGTH/1000)
-        void randVarVals (int n, vector<string>& name, vector<vector<value_t<int>>>& values) {
+        void randVarIntVals (int n, vector<string>& name, vector<vector<value_t<int>>>& values) {
 
             // first, randomly select a name for a variable
             name = randVectorString (n, 10);
@@ -61,18 +61,67 @@ class ManagerFixture : public ::testing::Test {
             }
         }
 
+        // return a randomly generated vector of strings and strings to populate
+        // the domain of each variable. The number of values generated is also
+        // randomly selected in the interval [0, MAX_LENGTH/1000)
+        void randVarStringVals (int n, vector<string>& name, vector<vector<value_t<string>>>& values) {
+
+            // first, randomly select a name for a variable
+            name = randVectorString (n, 10);
+
+            // and now randomly select integer values for the domain of each
+            // variable
+            for (int i = 0 ; i < n ; i++) {
+                vector<string> raw_values = randVectorString (1+rand ()% NB_VALUES, 20);
+
+                // and now transform these values into MUX values
+                vector<value_t<string>> ivalues;
+                for (auto value : raw_values) {
+                    ivalues.push_back (value_t<string>(value));
+                }
+
+                // and add the domain of the i-th variable
+                values.push_back (ivalues);
+            }
+        }
+
+        // return a randomly generated vector of strings and times to populate
+        // the domain of each variable. The number of values generated is also
+        // randomly selected in the interval [0, MAX_LENGTH/1000)
+        void randVarTimeVals (int n, vector<string>& name, vector<vector<value_t<time_t>>>& values) {
+
+            // first, randomly select a name for a variable
+            name = randVectorString (n, 10);
+
+            // and now randomly select integer values for the domain of each
+            // variable
+            for (int i = 0 ; i < n ; i++) {
+                vector<time_t> raw_values = randVectorTime (1+rand ()% NB_VALUES, time (nullptr));
+
+                // and now transform these values into MUX values
+                vector<value_t<time_t>> ivalues;
+                for (auto value : raw_values) {
+                    ivalues.push_back (value_t<time_t>(value));
+                }
+
+                // and add the domain of the i-th variable
+                values.push_back (ivalues);
+            }
+        }
+
         // given two locations of a matrix of instances of value_t return a
         // vector of pairs with the cross product of all values
-        vector<pair<int,int>> cross_product (int idx1, int idx2,
-                                             vector<vector<value_t<int>>>& values) {
+        template<typename T>
+        vector<pair<T, T>> cross_product (int idx1, int idx2,
+                                          vector<vector<value_t<T>>>& values) {
 
             // initialize the output
-            vector<pair<int, int>> output;
+            vector<pair<T, T>> output;
 
             // compute the cross product
             for (auto val1 : values[idx1]) {
                 for (auto val2 : values[idx2]) {
-                    output.push_back (pair<int, int> {val1.get_value (),
+                    output.push_back (pair<T, T> {val1.get_value (),
                         val2.get_value ()});
                 }
             }
@@ -83,7 +132,8 @@ class ManagerFixture : public ::testing::Test {
 
         // return a pair with the first and last index to the values in the
         // domain of the i-th variable given a matrix of values
-        pair<int, int> get_bounds (size_t i, vector<vector<value_t<int>>>& values) {
+        template<typename T>
+        pair<int, int> get_bounds (size_t i, vector<vector<value_t<T>>>& values) {
 
             int first = 0, last;;
 
--- tests/solver/TSTmanager.cc
+++ tests/solver/TSTmanager.cc
@@ -42,7 +42,7 @@ TEST_F (ManagerFixture, AddVariablesManager) {
         // randomly pick up information for the variables to insert
         vector<string> names;
         vector<vector<value_t<int>>> values;
-        randVarVals (NB_VARIABLES, names, values);
+        randVarIntVals (NB_VARIABLES, names, values);
 
         // now, insert all these variables
         for (int i = 0 ; i < NB_VARIABLES ; i++) {
@@ -91,9 +91,54 @@ TEST_F (ManagerFixture, AddVariablesManager) {
     }
 }
 
-vector<pair<int, int>> parameters;
+// Checks that posting constraints on unregistered variables raises an exception
+// ----------------------------------------------------------------------------
+TEST_F (ManagerFixture, UnregisteredVariablesManager) {
 
-// Checks that constraints are invoked using the right order of values. In
+    for (auto i = 0 ; i < NB_TESTS/100 ; i++) {
+
+        // create an empty table of CSP variables, i.e., with no values at all
+        manager<int> m;
+
+        // randomly pick up information for the variables to insert
+        vector<string> names;
+        vector<vector<value_t<int>>> values;
+        randVarIntVals (NB_VARIABLES/10, names, values);
+
+        // now, insert all these variables
+        for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
+
+            // add the i-th variable along with its domain
+            m.add_variable (variable_t (names[i]), values[i]);
+        }
+
+        // create two variables, one at least being unregistered. For this,
+        // throw a dice and decide among different cases
+        variable_t variable0("This variable is not expected to exist!");
+        variable_t variable1("This variable is not expected to exist as well!");
+        int basecase = rand () % 3;
+        switch (basecase) {
+            case 1:
+                variable1 = variable_t (names[rand () % NB_VARIABLES/10]);
+                break;
+            case 2:
+                variable0 = variable_t (names[rand () % NB_VARIABLES/10]);
+                break;
+        }
+
+        // Add now a dummy constraint (in the form of a lambda function) but
+        // invoke it over non-existing variables and verify the operation raises
+        // an exception
+        ASSERT_THROW(m.add_constraint([] (int val1, int val2) {
+            return true;
+        }, variable0, variable1),
+            invalid_argument);
+    }
+}
+
+vector<pair<int, int>> parametersInt;
+
+// Checks that constraints are invoked using the right order of int values. In
 // passing, verify that adding variables after posting constraints raises an
 // exception
 // ----------------------------------------------------------------------------
@@ -107,7 +152,7 @@ TEST_F (ManagerFixture, InvokeConstraintIntManager) {
         // randomly pick up information for the variables to insert
         vector<string> names;
         vector<vector<value_t<int>>> values;
-        randVarVals (NB_VARIABLES/10, names, values);
+        randVarIntVals (NB_VARIABLES/10, names, values);
 
         // now, insert all these variables
         for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
@@ -121,9 +166,9 @@ TEST_F (ManagerFixture, InvokeConstraintIntManager) {
 
         // Add now a constraint (in the form of a lambda function) which stores
         // all combinations of values as pairs
-        parameters.clear ();
+        parametersInt.clear ();
         m.add_constraint([] (int val1, int val2) {
-            parameters.push_back (pair<int, int>{val1, val2});
+            parametersInt.push_back (pair<int, int>{val1, val2});
             return true;
         },variable_t{names[variables[0]]}, variable_t{names[variables[1]]});
 
@@ -138,50 +183,14 @@ TEST_F (ManagerFixture, InvokeConstraintIntManager) {
                       runtime_error);
 
         // finally, verify that this is the expected cross-product
-        ASSERT_EQ (parameters, cross_product (variables[0], variables[1], values));
-    }
-}
-
-// Checks that posting constraints on unregistered variables raises an exception
-// ----------------------------------------------------------------------------
-TEST_F (ManagerFixture, UnregisteredVariableMutexIntManager) {
-
-    for (auto i = 0 ; i < NB_TESTS/100 ; i++) {
-
-        // create an empty table of CSP variables, i.e., with no values at all
-        manager<int> m;
-
-        // randomly pick up information for the variables to insert
-        vector<string> names;
-        vector<vector<value_t<int>>> values;
-        randVarVals (NB_VARIABLES/10, names, values);
-
-        // now, insert all these variables
-        for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
-
-            // add the i-th variable along with its domain
-            m.add_variable (variable_t (names[i]), values[i]);
-        }
-
-        // create two variables which are guaranteed (randomly though) to not
-        // exist
-        variable_t variable0 = variable_t ("This variable is not expected to exist!");
-        variable_t variable1 = variable_t ("This variable is not expected to exist as well!");
-
-        // Add now a dummy constraint (in the form of a lambda function) but
-        // invoke it over non-existing variables and verify the operation raises
-        // an exception
-        ASSERT_THROW(m.add_constraint([] (int val1, int val2) {
-            return true;
-        }, variable0, variable1),
-            invalid_argument);
+        ASSERT_EQ (parametersInt, cross_product<int> (variables[0], variables[1], values));
     }
 }
 
 int quotient;
 
 // Checks that constraints properly acknowledge mutexes and store them
-// accordingly
+// accordingly in managers of integers
 // ----------------------------------------------------------------------------
 TEST_F (ManagerFixture, MutexIntManager) {
 
@@ -193,7 +202,7 @@ TEST_F (ManagerFixture, MutexIntManager) {
         // randomly pick up information for the variables to insert
         vector<string> names;
         vector<vector<value_t<int>>> values;
-        randVarVals (NB_VARIABLES/10, names, values);
+        randVarIntVals (NB_VARIABLES/10, names, values);
 
         // now, insert all these variables
         for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
@@ -226,8 +235,8 @@ TEST_F (ManagerFixture, MutexIntManager) {
 
         // get the index to the first and last value in the domain of each
         // variable
-        pair<int, int> bounds1 = get_bounds (variables[0], values);
-        pair<int, int> bounds2 = get_bounds (variables[1], values);
+        pair<int, int> bounds1 = get_bounds<int> (variables[0], values);
+        pair<int, int> bounds2 = get_bounds<int> (variables[1], values);
 
         // to double check that the boundaries have been properly computed,
         // assert their equality
@@ -263,7 +272,261 @@ TEST_F (ManagerFixture, MutexIntManager) {
     }
 }
 
+vector<pair<string, string>> parametersString;
 
+// Checks that constraints are invoked using the right order of string values.
+// In passing, verify that adding variables after posting constraints raises an
+// exception
+// ----------------------------------------------------------------------------
+TEST_F (ManagerFixture, InvokeConstraintStringManager) {
+
+    for (auto i = 0 ; i < NB_TESTS/100 ; i++) {
+
+        // create an empty table of CSP variables, i.e., with no values at all
+        manager<string> m;
+
+        // randomly pick up information for the variables to insert
+        vector<string> names;
+        vector<vector<value_t<string>>> values;
+        randVarStringVals (NB_VARIABLES/10, names, values);
+
+        // now, insert all these variables
+        for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
+
+            // add the i-th variable along with its domain
+            m.add_variable (variable_t (names[i]), values[i]);
+        }
+
+        // randomly choose two different variables (which could be the same ...)
+        auto variables = randVectorInt (2, NB_VARIABLES/10);
+
+        // Add now a constraint (in the form of a lambda function) which stores
+        // all combinations of values as pairs
+        parametersString.clear ();
+        m.add_constraint([] (string val1, string val2) {
+            parametersString.push_back (pair<string, string>{val1, val2});
+            return true;
+        },variable_t{names[variables[0]]}, variable_t{names[variables[1]]});
+
+        // before making the important assertion, ensure also that after posting
+        // constraints, the multibitmap is not null anymore
+        ASSERT_NE (m.get_multibmap(), nullptr);
+
+        // and that posting additional constraints automatically raises an
+        // exception
+        ASSERT_THROW (m.add_variable (variable_t ("not allowed!"),
+                                      values[0]),
+                      runtime_error);
+
+        // finally, verify that this is the expected cross-product
+        ASSERT_EQ (parametersString, cross_product<string> (variables[0], variables[1], values));
+    }
+}
+
+// Checks that constraints properly acknowledge mutexes and store them
+// accordingly in managers of strings
+// ----------------------------------------------------------------------------
+TEST_F (ManagerFixture, MutexStringManager) {
+
+    for (auto i = 0 ; i < NB_TESTS/100 ; i++) {
+
+        // create an empty table of CSP variables, i.e., with no values at all
+        manager<string> m;
+
+        // randomly pick up information for the variables to insert
+        vector<string> names;
+        vector<vector<value_t<string>>> values;
+        randVarStringVals (NB_VARIABLES/10, names, values);
+
+        // now, insert all these variables
+        for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
+
+            // add the i-th variable along with its domain
+            m.add_variable (variable_t (names[i]), values[i]);
+        }
+
+        // randomly choose two different variables (which could be the same ...)
+        auto variables = randVectorInt (2, NB_VARIABLES/10);
+
+        // Add now a constraint (in the form of a lambda function) which returns
+        // false if any of the strings contains the letter 'r'
+        m.add_constraint([] (string val1, string val2) {
+            return !val1.find ('r') && !val2.find ('r');
+        }, variable_t{names[variables[0]]}, variable_t{names[variables[1]]});
+
+        // now, process all values of both variables and verify that mutexes
+        // have been properly recognized
+
+        // access the manager multibitmap. It is guarantted to be non-null.
+        // Indeed one unit test before this one already verified this
+        const multibmap_t* multibmap = m.get_multibmap();
+
+        // get the index to the first and last value in the domain of each
+        // variable
+        pair<int, int> bounds1 = get_bounds<string> (variables[0], values);
+        pair<int, int> bounds2 = get_bounds<string> (variables[1], values);
+
+        // to double check that the boundaries have been properly computed,
+        // assert their equality
+        ASSERT_EQ (m.get_vartable().get_first(variables[0]),
+                   bounds1.first);
+        ASSERT_EQ (m.get_vartable().get_last(variables[0]),
+                   bounds1.second);
+        ASSERT_EQ (m.get_vartable().get_first(variables[1]),
+                   bounds2.first);
+        ASSERT_EQ (m.get_vartable().get_last(variables[1]),
+                   bounds2.second);
+
+        // and now verify the values of all mutexies
+        for (int idx1 = bounds1.first ; idx1 < bounds1.second ; idx1++) {
+            for (int idx2 = bounds2.first ; idx2 < bounds2.second ; idx2++) {
+
+                // get the values stored in the multibitmap
+                string str1 = values[variables[0]][idx1-bounds1.first].get_value ();
+                string str2 = values[variables[1]][idx2-bounds2.first].get_value ();
+
+                // and verify that only those cases where either argument
+                // contains the letter 'r' is recognized as a mutex
+                ASSERT_EQ (multibmap->get (idx1, idx2),
+                           str1.find ('r') || str2.find ('r'));
+            }
+        }
+    }
+}
+
+vector<pair<time_t, time_t>> parametersTime;
+
+// Checks that constraints are invoked using the right order of time values.
+// In passing, verify that adding variables after posting constraints raises an
+// exception
+// ----------------------------------------------------------------------------
+TEST_F (ManagerFixture, InvokeConstraintTimeManager) {
+
+    for (auto i = 0 ; i < NB_TESTS/100 ; i++) {
+
+        // create an empty table of CSP variables, i.e., with no values at all
+        manager<time_t> m;
+
+        // randomly pick up information for the variables to insert
+        vector<string> names;
+        vector<vector<value_t<time_t>>> values;
+        randVarTimeVals (NB_VARIABLES/10, names, values);
+
+        // now, insert all these variables
+        for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
+
+            // add the i-th variable along with its domain
+            m.add_variable (variable_t (names[i]), values[i]);
+        }
+
+        // randomly choose two different variables (which could be the same ...)
+        auto variables = randVectorInt (2, NB_VARIABLES/10);
+
+        // Add now a constraint (in the form of a lambda function) which stores
+        // all combinations of values as pairs
+        parametersTime.clear ();
+        m.add_constraint([] (time_t val1, time_t val2) {
+            parametersTime.push_back (pair<time_t, time_t>{val1, val2});
+            return true;
+        },variable_t{names[variables[0]]}, variable_t{names[variables[1]]});
+
+        // before making the important assertion, ensure also that after posting
+        // constraints, the multibitmap is not null anymore
+        ASSERT_NE (m.get_multibmap(), nullptr);
+
+        // and that posting additional constraints automatically raises an
+        // exception
+        ASSERT_THROW (m.add_variable (variable_t ("not allowed!"),
+                                      values[0]),
+                      runtime_error);
+
+        // finally, verify that this is the expected cross-product
+        ASSERT_EQ (parametersTime, cross_product<time_t> (variables[0], variables[1], values));
+    }
+}
+
+// Checks that constraints properly acknowledge mutexes and store them
+// accordingly in managers of time
+// ----------------------------------------------------------------------------
+TEST_F (ManagerFixture, MutexTimeManager) {
+
+    for (auto i = 0 ; i < NB_TESTS/100 ; i++) {
+
+        // create an empty table of CSP variables, i.e., with no values at all
+        manager<time_t> m;
+
+        // randomly pick up information for the variables to insert
+        vector<string> names;
+        vector<vector<value_t<time_t>>> values;
+        randVarTimeVals (NB_VARIABLES/10, names, values);
+
+        // now, insert all these variables
+        for (int i = 0 ; i < NB_VARIABLES/10 ; i++) {
+
+            // add the i-th variable along with its domain
+            m.add_variable (variable_t (names[i]), values[i]);
+        }
+
+        // randomly choose two different variables (which could be the same ...)
+        auto variables = randVectorInt (2, NB_VARIABLES/10);
+
+        // Add now a constraint (in the form of a lambda function) which returns
+        // false if the sum of its arguments is divisible by a number between 1
+        // and 5 ---in case 1 is selected, all combinations are then mutex--- or
+        // a very large constant so that no number is selected as a mutex
+        quotient = 1 + rand ()%6;
+        m.add_constraint([] (time_t val1, time_t val2) {
+            if (quotient == 6) {
+                return true;
+            }
+            return (val1 + val2) % quotient != 0;
+        }, variable_t{names[variables[0]]}, variable_t{names[variables[1]]});
+
+        // now, process all values of both variables and verify that mutexes
+        // have been properly recognized
+
+        // access the manager multibitmap. It is guarantted to be non-null.
+        // Indeed one unit test before this one already verified this
+        const multibmap_t* multibmap = m.get_multibmap();
+
+        // get the index to the first and last value in the domain of each
+        // variable
+        pair<int, int> bounds1 = get_bounds<time_t> (variables[0], values);
+        pair<int, int> bounds2 = get_bounds<time_t> (variables[1], values);
+
+        // to double check that the boundaries have been properly computed,
+        // assert their equality
+        ASSERT_EQ (m.get_vartable().get_first(variables[0]),
+                   bounds1.first);
+        ASSERT_EQ (m.get_vartable().get_last(variables[0]),
+                   bounds1.second);
+        ASSERT_EQ (m.get_vartable().get_first(variables[1]),
+                   bounds2.first);
+        ASSERT_EQ (m.get_vartable().get_last(variables[1]),
+                   bounds2.second);
+
+        // and now verify the values of all mutexies
+        for (int idx1 = bounds1.first ; idx1 < bounds1.second ; idx1++) {
+            for (int idx2 = bounds2.first ; idx2 < bounds2.second ; idx2++) {
+
+                // get the values stored in the multibitmap
+                size_t num1 = values[variables[0]][idx1-bounds1.first].get_value ();
+                size_t num2 = values[variables[1]][idx2-bounds2.first].get_value ();
+
+                // and verify that only those cases where the sum is divisible
+                // by 7 have been recognized as mutexes. Note the case
+                // quotient=6 is also acknowledged as in this case, no pair is
+                // mutex
+                if (quotient==6) {
+                    ASSERT_FALSE (multibmap->get (idx1, idx2));
+                } else {
+                    ASSERT_EQ (multibmap->get (idx1, idx2),
+                               (num1 + num2) % quotient == 0);
+                }
+            }
+        }
+    }
+}
 
 // Local Variables:
 // mode:cpp
